0297da5a3d8c0bd159e0a1d956f89fc2850217b6
Kyrylo Kalashnikov <kir486680@gmail.com>
2024-03-29 13:53:23 -0400

    remove numpy

--- test/test.py
+++ test/test.py
@@ -1,14 +1,13 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, Edge
-
-
-import numpy as np
+import struct
 
 def float16_to_8bit_bytes(value):
-    if not isinstance(value, np.float16):
-        raise TypeError("Input must be a numpy float16.")
-    int_representation = np.frombuffer(value.tobytes(), dtype=np.uint16)[0]
+    if not isinstance(value, float):
+        raise TypeError("Input must be a float.")
+    binary_representation = struct.pack('>e', value)
+    int_representation = int.from_bytes(binary_representation, byteorder='big', signed=False)
     high_byte = int_representation >> 8
     low_byte = int_representation & 0xFF
     return (high_byte, low_byte)
@@ -19,17 +18,15 @@ def binary_strings_to_float16(high_byte_str, low_byte_str):
     high_byte = int(high_byte_str, 2)
     low_byte = int(low_byte_str, 2)
     int_representation = (high_byte << 8) | low_byte
-    float_value = np.frombuffer(np.array([int_representation], dtype=np.uint16).tobytes(), dtype=np.float16)[0]
+    binary_representation = int_representation.to_bytes(2, byteorder='big', signed=False)
+    float_value = struct.unpack('>e', binary_representation)[0]
     return float_value
-
-
 
 @cocotb.test()
 async def test_neural_chip(dut):
     clock_period = 83.33
     clock = Clock(dut.clk, clock_period, units="ns")
     cocotb.start_soon(clock.start())
-
     dut.rst_n.value = 0
     await RisingEdge(dut.clk)
     await RisingEdge(dut.clk)
@@ -43,9 +40,8 @@ async def test_neural_chip(dut):
 
     # Generate and send float16 values
     for i in range(8):
-        float16 = np.float16(i+1)
+        float16 = float(i+1)
         high, low = float16_to_8bit_bytes(float16)
-
         # Send the high byte of the float
         await send_byte(dut, format(high, '08b'))
         await Timer(104.167, units='ns')
@@ -58,21 +54,17 @@ async def test_neural_chip(dut):
     print(dut.uo_out[1].value)
     assert dut.uo_out[1].value == 1
 
-
 async def send_byte(dut, byte):
     print("Byte to send:", byte)
-
     # Send start bit
     dut.ui_in[0].value = 0
     print("Sending start bit")
     await Timer(104.167, units='us')  # Delay for 1 bit time at 9600 baud
-
     # Send data bits
     for i in range(7, -1, -1):
         dut.ui_in[0].value = int(byte[i])
         print(f"Sending bit {7-i+1}: {dut.ui_in[0].value}")
         await Timer(104.167, units='us')  # Delay for 1 bit time at 9600 baud
-
     # Send stop bit
     dut.ui_in[0].value = 1
     print("Sending stop bit")
