84fdaac357133630f0f83ae7d16d9fba5ba8289f
Kyrylo Kalashnikov <kir486680@gmail.com>
2024-03-30 17:13:11 -0400

    edit to the pure chip logic

--- src/tt_um_neurocore.v
+++ src/tt_um_neurocore.v
@@ -20,13 +20,12 @@ module tt_um_neurocore #( parameter MAX_COUNT = 24'd10_000_000 ) (
         .CLK(clk),
         .RESET(rst_n),
         .RXD(ui_in[0]),
-        .received_state(uo_out[7:3]),
-        .load_arr(uo_out[2]),
+        .logs(uo_out[7:0]),
         .rx_data_test(uio_out)
     );
 
     // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out[1:0] = 2'b0;  // Assign ou_out[7:3] to 0 as it's not used for output
+    //assign uo_out[1:0] = 2'b0;  // Assign ou_out[7:3] to 0 as it's not used for output
     //assign uio_out[7:0] = 8'b0;     // Assign uio_out to 0 as it's not used for output
     assign uio_oe [7:0]= 8'b0;      // Assign uio_oe to 0 to configure uio pins as input or disable them for output
 
