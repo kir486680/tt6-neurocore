2691f3d60541c7c8733236a1d7ec10411b2be217
Kyrylo Kalashnikov <kir486680@gmail.com>
2024-03-30 20:48:00 -0400

    working with the clock with pure chip

--- test/test.py
+++ test/test.py
@@ -3,25 +3,6 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, ClockCycles, Timer, Edge
 import struct
 
-def float16_to_8bit_bytes(value):
-    if not isinstance(value, float):
-        raise TypeError("Input must be a float.")
-    binary_representation = struct.pack('>e', value)
-    int_representation = int.from_bytes(binary_representation, byteorder='big', signed=False)
-    high_byte = int_representation >> 8
-    low_byte = int_representation & 0xFF
-    return (high_byte, low_byte)
-
-def binary_strings_to_float16(high_byte_str, low_byte_str):
-    if len(high_byte_str) != 8 or len(low_byte_str) != 8:
-        raise ValueError("Both strings must be 8 bits long.")
-    high_byte = int(high_byte_str, 2)
-    low_byte = int(low_byte_str, 2)
-    int_representation = (high_byte << 8) | low_byte
-    binary_representation = int_representation.to_bytes(2, byteorder='big', signed=False)
-    float_value = struct.unpack('>e', binary_representation)[0]
-    return float_value
-
 @cocotb.test()
 async def test_neural_chip(dut):
 
@@ -30,8 +11,7 @@ async def test_neural_chip(dut):
     cocotb.start_soon(clock.start())
 
     dut.rst_n.value = 0
-    await RisingEdge(dut.clk)
-    await RisingEdge(dut.clk)
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
     await RisingEdge(dut.clk)
     await RisingEdge(dut.clk)
