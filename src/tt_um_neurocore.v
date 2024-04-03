fe72a0c3c5e351dcafe7ef2c3714faa9098c37a5
Kyrylo Kalashnikov <kir486680@gmail.com>
2024-04-03 17:34:51 -0400

    dummy testing of uio_in

--- src/tt_um_neurocore.v
+++ src/tt_um_neurocore.v
@@ -19,7 +19,7 @@ module tt_um_neurocore #( parameter MAX_COUNT = 24'd10_000_000 ) (
     NeuralChip neural_chip_inst (
         .CLK(clk),
         .RESET(rst_n),
-        .RXD(ui_in[0]),
+        .RXD(uio_in[0]),
         .logs(uo_out[7:0]),
         .rx_data_test(uio_out)
     );
@@ -27,7 +27,7 @@ module tt_um_neurocore #( parameter MAX_COUNT = 24'd10_000_000 ) (
     // All output pins must be assigned. If not used, assign to 0.
     //assign uo_out[1:0] = 2'b0;  // Assign ou_out[7:3] to 0 as it's not used for output
     //assign uio_out[7:0] = 8'b0;     // Assign uio_out to 0 as it's not used for output
-    assign uio_oe [7:0]= 8'b0;      // Assign uio_oe to 0 to configure uio pins as input or disable them for output
+    assign uio_oe = 0;      // Assign uio_oe to 0 to configure uio pins as input or disable them for output 
 
 endmodule
 
