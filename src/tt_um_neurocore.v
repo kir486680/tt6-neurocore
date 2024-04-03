ad551b550dbd7b58abbb26219492a04306e6bb94
Kyrylo Kalashnikov <kir486680@gmail.com>
2024-04-03 18:08:15 -0400

    testing dummy

--- src/tt_um_neurocore.v
+++ src/tt_um_neurocore.v
@@ -18,20 +18,26 @@ module tt_um_neurocore #( parameter MAX_COUNT = 24'd10_000_000 ) (
 // Hypothesis: the ports are not connected correctly. try different permutations of the io's and see if the testbench can drive the signals
 // Procedure: try uio_in and uio_out
 
+    reg [7:0] output_reg;
+    assign uo_out = output_reg;
 
-    NeuralChip neural_chip_inst (
-        .CLK(clk),
-        .RESET(rst_n),
-        .RXD(uio_in[0]),
-        .logs(uio_out),
-        .rx_data_test(uo_out[7:0])
-    );
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            output_reg <= 2;
+        end else if (uio_in[0]) begin
+            output_reg <= 8;
+        end
+    end
+
+    
+    
+
 
     // All output pins must be assigned. If not used, assign to 0.
     //assign uo_out[1:0] = 2'b0;  // Assign ou_out[7:3] to 0 as it's not used for output
     //assign uio_out[7:0] = 8'b0;     // Assign uio_out to 0 as it's not used for output
     assign uio_oe = 0;      // Assign uio_oe to 0 to configure uio pins as input or disable them for output 
-
+    assign uio_out = 0;      // Assign uo_out to 0 to configure uo pins as output or disable them for input
 endmodule
 
 
